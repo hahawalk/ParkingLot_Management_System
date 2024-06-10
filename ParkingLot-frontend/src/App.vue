00bd2b76e367f824741732ebfe72583c7378d817
hahawalk <157509280+hahawalk@users.noreply.github.com>
2024-06-10 15:04:48 +0800

    Add files via upload

---
+++ ParkingLot-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * 应用入口
+ *
+ * @Author: walk
+ * @Date: 2024-5-25
+-->
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+#app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+</style>
---
+++ ParkingLot-frontend/src/components/Home.vue
@@ -0,0 +1,46 @@
+<!--
+ * 系统首页
+ *
+ * @Author: walk
+ * @Date: 2024-5-25
+-->
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item
+        ><i
+          class="iconfont icon-home-menu"
+          style="margin: 5px; font-size: 22px"
+        >
+          首页</i
+        ></el-breadcrumb-item
+      >
+    </el-breadcrumb>
+    <el-row style="background-color: white; padding: 0 0 50px 25px">
+      <el-col
+        :span="24"
+        style="font-size: 24px; text-align: center; color: #3eb628"
+      >
+        <h1>欢迎访问停车场管理系统</h1>
+      </el-col>
+
+      <el-col :span="18" style="text-align: center">
+        <img
+          :src="require('@/assets/img/03.jpg')"
+          style="text-align: center; height: 100vh; width: 85vw"
+        />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+<style scoped>
+</style>
